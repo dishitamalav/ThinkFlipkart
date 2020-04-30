410d78d672952f1fae6ab8fb5d040ceb795b277c
dishitamalav <39398125+dishitamalav@users.noreply.github.com>
2020-04-30 12:58:17 +0530

    Update String.cpp
    Added to upper to string.cpp

--- SourceCode/String/String.cpp
+++ SourceCode/String/String.cpp
@@ -4,6 +4,10 @@ void greetings(char hello[]){
 	printf("\n%s",hello);
 }
 
+void toUpper(char string[]){
+	printf("toupper);
+	       }
+
 int main(){
 	char greet[] = "hello world";
 	greetings(greeting);
